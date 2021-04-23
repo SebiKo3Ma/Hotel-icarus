50d723eb34ea40fff54dbdc1849676bdb994be62
SebiKo3Ma <sebi.sam.cozma@gmail.com>
2021-04-23 22:10:04 +0300

    part 2

--- Assets/scripts/dropdown.js
+++ Assets/scripts/dropdown.js
@@ -1,3 +1,18 @@
+var drop = 0;
+
 document.getElementById('dropdown').onclick = function(){
-  
-}
+  if(drop == 0)
+  {
+    document.getElementById("menuSelectors").className = "menuSelectors";
+      drop = 1;
+  }
+  else {
+
+    if(drop == 1)
+    {
+      drop = 0;
+      document.getElementById("menuSelectors").className = "menuSelectors showMenuFalse";
+    }
+
+  }
+};
